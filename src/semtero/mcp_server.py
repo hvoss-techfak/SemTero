e8381dd9ca1ea9394da8253daa8a25589bcad714
hvoss <hvoss@techfak.uni-bielefeld.de>
2026-03-09 21:04:33 +0100

    increased top_sentences for better rag

--- src/semtero/mcp_server.py
+++ src/semtero/mcp_server.py
@@ -331,7 +331,7 @@ class MCPZoteroServer:
             except Exception:
                 logger.debug("Search progress callback failed", exc_info=True)
 
-        temp_top_sentences = max(500, top_sentences)
+        temp_top_sentences = max(100, top_sentences)
 
         emit_progress(
             {
